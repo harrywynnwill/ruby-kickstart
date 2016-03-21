4daee4fc088e6e27b65858d99c3ab3dcdf210f55
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-03-21 21:22:36 +0000

    1:4 commit university grades book numbers and boolean using if else with nested switch

--- session1/challenge/4_logic.rb
+++ session1/challenge/4_logic.rb
@@ -1,6 +1,6 @@
 # A grad student at a local university thinks he has discovered a formula to
-# predict what kind of grades a person will get. He says if you own less than 
-# 10 books, you will get a "D". If you own 10 to 20 books, you will get a "C", 
+# predict what kind of grades a person will get. He says if you own less than
+# 10 books, you will get a "D". If you own 10 to 20 books, you will get a "C",
 # and if you own more than 20 books, you will get a "B".
 # He further hypothesizes that if you actually read your books, then you will
 # get a full letter grade higher in every case.
@@ -10,4 +10,28 @@
 # grade(15, true)   # => "B"
 
 def grade(num_books, reads_books)
+    if reads_books == false #if the books not read falls into first switch else falls into the other switch with 1 grade more.
+      case num_books
+      when 0..9
+      "D"
+      when 10..20
+      "C"
+      when 21..30
+      "B"
+      else
+      "B"
+      end
+    else
+      case num_books
+      when 0..9
+      "C"
+      when 10..20
+      "B"
+      when 21..30
+      "A"
+      else
+      "A"
+      end
+ end
 end
+
