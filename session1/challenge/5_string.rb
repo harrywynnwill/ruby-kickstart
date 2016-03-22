301f7aec23ad4ae384103d9b6bc7d6f2bd79fa60
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-03-22 12:24:13 +0000

    1:5 and 1:6 commited

--- session1/challenge/5_string.rb
+++ session1/challenge/5_string.rb
@@ -1,8 +1,16 @@
 # Given a string, replace every instance of sad to happy
-# 
+#
 # add_more_ruby("The clowns were sad.")         # => "The clowns were happy."
 # add_more_ruby("The sad dad said sad stuff.")  # => "The happy dad said happy stuff."
 # add_more_ruby("Sad times are ahead!")         # => "Happy times are ahead!"
-
+=begin
 def add_more_ruby(string)
+  if string == string.capitalize
+  string.gsub(/sad/i,"happy")
+  end
+  string.gsub(/sad/i,"happy")
+end
+=end
+def add_more_ruby(string)
+  string.gsub('sad', 'happy').gsub('Sad', 'Happy')
 end
--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -1,11 +1,28 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-# 
+#
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-# 
+#
 # odds_and_evens("abcdefg",true)    # => "bdf"
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
+
+
 def odds_and_evens(string, return_odds)
+ letters=string.split"" #split returns an array, no need to pass to an array
+ if return_odds == true
+    letters.map!.each_with_index {|item ,index| item if index.odd?}
+    letters.join
+ else
+  letters.map!.each_with_index {|item ,index| item if index.even?}
+  letters.join
+ end
 end
+
+
+
+
+
+#odds_and_evens("abcdefg",true)    # => "bdf"
+#odds_and_evens("abcdefg",false)   # => "aceg"
