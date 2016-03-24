3cc1916fbd1ceaa0282a5d743be7061354b562b7
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-03-24 13:03:07 +0000

    git commit 2:4 && 2:5

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -4,6 +4,13 @@
 # get_squares [9]                      # => []
 # get_squares [9,3]                    # => [3]
 # get_squares [9,3,81]                 # => [3, 9]
-# get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
+ #get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+def get_squares (numbers)
+   
+   numbers.select{|x| numbers.include? x*x}.map{|x| p x}.sort
+   
+  end 
+  
+get_squares [25, 4, 9, 6, 50, 16, 5]
--- session2/challenge/5_array.rb
+++ session2/challenge/5_array.rb
@@ -13,3 +13,11 @@
 # mod_three [7]  # => [1]
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
+
+def mod_three (array)
+ array.select  {|x| x!=0 && x%3!=0 }.map{|x| x % 3}
+ 
+  
+end
+#mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
+mod_three [4]  # => [1]
