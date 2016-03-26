e86fd6532f44b8c4e3ad992dba3cf9ead3c06103
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-03-26 11:24:50 +0000

    2:12 completed

--- session2/challenge/12_classes.rb
+++ session2/challenge/12_classes.rb
@@ -21,8 +21,42 @@
 # f.to_f               # => 0.5
 
 class Fraction
+  def initialize (numerator, denominator)
+    @numerator = numerator
+    @denominator = denominator
+  end 
+    
+  attr_accessor :numerator, :denominator
   def gcd(a,b)
     return a if b == 0
     gcd(b, a%b)
   end
+  def to_f
+    @numerator/@denominator.to_f
+  end
+  def to_s
+    "#{@numerator}/#{@denominator}"
+  end
+  def lowest
+   divisor = gcd(@numerator, @denominator) 
+   x = (@numerator / divisor)
+   y = (@denominator/ divisor)
+   Fraction.new(x, y)
+    #"#{x}/#{y}"
+  #@denominator/divisor
+  end
 end
+
+
+f = Fraction.new 20, 60
+
+p f.numerator          # => 20
+p f.denominator        # => 60
+p f.to_s               # => "20/60"
+p f.lowest.to_s        # => "1/3"
+p f.numerator = 50
+p f.denominator = 100
+p f = f.lowest
+p f.to_s               # => "50/100"
+p f.to_f               # => 0.5
+ 
