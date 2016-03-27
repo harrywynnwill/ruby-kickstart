4c29ddd59a8cb60d1da4ff4231e8d885ccab6ec5
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-03-27 16:04:10 +0100

    2:7 complete

--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -8,3 +8,17 @@
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+
+def alternate_words (sentence)
+  '!@$#%^&*()-=_+[]:;,./<>?\\|'.split(//).each {|char|
+    sentence = sentence.gsub(char, ' ')}
+    words = sentence.split
+    solution = []
+    words.each_with_index {|word, index|
+      solution << word if index.even?
+    }  
+   p solution 
+  
+end
+p alternate_words("Lorem ipsum dolor sit amet.")
+ alternate_words("Can't we all get along?")  
