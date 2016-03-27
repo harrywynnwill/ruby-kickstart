40f027293b74397a24159f06bef86f797c083f75
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-03-27 15:34:53 +0100

    1:7 complete

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -4,5 +4,19 @@
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
-def pirates_say_arrrrrrrrr(string)
-end
+
+  def pirates_say_arrrrrrrrr(string)
+    letters = string.split""  
+    letters.map!.each_with_index do |item, index |  
+      if item == "r" || item == "R"
+    letters[index+1]
+      
+      
+     
+      else
+       ""
+      end
+    end
+    letters.join
+  end
+p pirates_say_arrrrrrrrr("are you really learning Ruby?") 
