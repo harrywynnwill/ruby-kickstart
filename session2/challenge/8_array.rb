2ff0275560bebb7f591888fcb107cecc2e974dca
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-03-28 09:40:48 +0100

    2:8 works for selecting arrays with 3 items occuring but not in a row

--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -1,7 +1,48 @@
 # Given an array of elements, return true if any element shows up three times in a row
-# 
+def got_three? (array)
+   counts = Hash.new 0
+  array.each do |x|
+    counts[x] += 1
+
+  end
+
+x = counts.map {|k,v| v
+
+}
+
+
+test = x.map.select {|x|
+   x >=3
+
+
+}
+
+test.empty? ? false : true
+
+=begin
+x = test.map.each { |e|
+  e.empty? p false
+
+elsif e.empty?
+
+end
+  }
+  #x >=3 ? test = true : test = false
+
+#}
+=end
+end
+
+
+
+
+
+
+
 # Examples:
-# got_three? [1, 2, 2, 2, 3]  # => true
-# got_three? ['a', 'a', 'b']  # => false
-# got_three? ['a', 'a', 'a']  # => true
-# got_three? [1, 2, 1, 1]     # => false
+p got_three? [1, 2,2,2, 3]  # => true
+ p got_three? ['a', 'a', 'b']  # => false
+p got_three? ['a', 'a', 'a']  # => true
+p got_three? [1, 2, 1]     # => false
+p got_three? [1,2,1,1]
+
